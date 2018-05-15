7d772952c85a68630e56b14d02d2389d063bb41e
Jerry Lee <lee@idoctor.co.jp>
2018-05-15 19:03:21 +0900

    fix long cmd's line wrapping bug

--- bash/my_shell.bash
+++ bash/my_shell.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Git Prompt
-__my_git_prompt() {
+__my_git_prompt_repo_name() {
   # inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+  # preserve exit status
+  local exit=$?
+  local printf_format='%s'
+  #local printf_format='[\033[36m%s \033[31m%s\033[0m]'
   if [ `git rev-parse --is-inside-work-tree 2>/dev/null` ]; then
     local repo_name=$(basename `git rev-parse --show-toplevel`)
-    local git_ps1=$(__git_ps1 %s)
-    #echo "[%{${fg[cyan]}%}$repo_name %{${fg[red]}%}$git_ps1%{${reset_color}%}]"
-    echo -e -n "[\033[36m$repo_name \033[31m$git_ps1\033[0m]"
+    # local git_ps1=$(__git_ps1 %s)
+    # echo "[%{${fg[cyan]}%}$repo_name %{${fg[red]}%}$git_ps1%{${reset_color}%}]"
+    # echo -e -n "[\033[36m$repo_name \033[31m$git_ps1\033[0m]"
+    printf -- "$printf_format" $repo_name
   else
-    echo ""
+    printf ""
   fi
+  return $exit
 }
 
 __source_git_helper() {
@@ -36,7 +42,8 @@ else
   curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > $HOME/.git_helper/git-completion.bash
   __source_git_helper
 fi
-export PS1='\[\033[32m\]\u@\h\[\033[0m\] \[\033[33m\]\W\[\033[0m\] $(__my_git_prompt)\$ '
+# export PS1='\[\033[32m\]\u@\h\[\033[0m\] \[\033[33m\]\W\[\033[0m\] $(__my_git_prompt)\$ '
+export PS1='\[\033[32m\]\u@\h\[\033[0m\] \[\033[33m\]\W\[\033[0m\] $(__git_ps1 "[\[\033[36m\]$(__my_git_prompt_repo_name) \[\033[31m\]%s\[\033[0m\]]")\$ '
 # export PS1='\[\033[32m\]\u \[\033[33m\]\w\[\033[0m\] $(__my_git_prompt)\$ '
 
 # Aliases
