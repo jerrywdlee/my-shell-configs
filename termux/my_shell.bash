9de5651dcdd252ab37a6a77de627dca86e3bfeea
Jerry Lee <lee@idoctor.co.jp>
2018-12-17 19:28:25 +0900

    add ngrok alias

--- termux/my_shell.bash
+++ termux/my_shell.bash
@@ -8,3 +8,8 @@ MY_SHELL_PATH=`dirname $(dirname $BASH_SOURCE)`
 . $MY_SHELL_PATH/lib/git_helper.sh
 
 export PS1='\[\033[32m\]\u@\h\[\033[0m\] \[\033[33m\]\W\[\033[0m\] $(__git_ps1 "[\[\033[36m\]$(__my_git_prompt_repo_name) \[\033[31m\]%s\[\033[0m\]]")\n\$ '
+
+# For loading applications
+if [ -f $HOME/.ngrok2/ngrok ]; then
+  alias ngrok="$HOME/.ngrok2/ngrok"
+fi
