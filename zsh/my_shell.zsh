7380f7ab7e703e9150ff34cfc6328b358455a2ee
Jerry Lee <jerrywdlee@gmail.com>
2020-03-04 18:47:45 +0900

    update settings

--- zsh/my_shell.zsh
+++ zsh/my_shell.zsh
@@ -2,7 +2,7 @@
 
 MY_SHELL_ZSH_ROOT=`dirname $0`
 MY_SHELL_ROOT=`dirname ${MY_SHELL_ZSH_ROOT}`
-GIT_HELPR_PATH=`${MY_SHELL_ROOT}/git_helper`
+GIT_HELPR_PATH="$MY_SHELL_ROOT/git_helper"
 ANTIGEN="$MY_SHELL_ZSH_ROOT/antigen.zsh"
 
 if [ ! -f "$ANTIGEN" ]; then
